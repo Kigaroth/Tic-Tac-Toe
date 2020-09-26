f7a1d6a912efd413b656e4176941416a6e7174ff
Kigaroth <hp_20@hotmail.com>
2020-09-27 00:03:05 +0200

    game works

--- Tic Tac Toe/Tic Tac Toe.cpp
+++ Tic Tac Toe/Tic Tac Toe.cpp
@@ -8,8 +8,7 @@
 #include <cstdlib>
 #include <ctime>
 
-//int//
-int number_of_players;
+int number_of_players = 0;
 char player = 'O';
 const int size = 9;
 
@@ -17,12 +16,12 @@ const int size = 9;
 int my_array[size] = {};
 std::vector <char> board = { '1','2','3','4','5','6','7','8','9' };
 
-char turn = 'X';
 
 bool game_won = false;
 bool game_draw = false;
+bool replay_game = true;
 
-//funtctions//
+//functions//
 void set_player() { //sets the players turn.
     if (player == 'O') { //sets player 1s turn
         player = 'X';
@@ -34,17 +33,6 @@ void set_player() { //sets the players turn.
     }
 }
 
-void set_ai() { //sets the players turn.
-    if (player == 'O') { //sets player 1s turn
-        player = 'X';
-        std::cout << "\n Player 1s turn.";
-    }
-    else { //sets AIs turn
-        player = 'O';
-        std::cout << "\n Computers turn.";
-    }
-}
-
 void user_input() { //let's the user input their choice:
 
     int player_input = 0;
@@ -52,25 +40,25 @@ void user_input() { //let's the user input their choice:
     bool input_exists = false;
 
     do {
-std::cout << " Please select a number 1 - 9: ";
-std::cin >> player_input;
+        std::cout << " Please select a number 1 - 9: ";
+        std::cin >> player_input;
 
-if (player_input == 0 || player_input > 9) { //Checks if user input is valid or not.
-    std::cout << "\n Invalid input, try again.";
-}
-
-else { //If user input is valid, the function swaps the number on the board to the users character (X or O depending on the player).
-    for (int i = 0; i < board.size(); i++) {
-        if (('0' + player_input) == board[i]) {
-            board[i] = player;
-            input_exists = true;
+        if (player_input == 0 || player_input > 9) { //Checks if user input is valid or not.
+              std::cout << "\n Invalid input, try again.";
         }
-    }
-}
 
-if (!input_exists) {
-    std::cout << "\n Box is already filled! Try again!\n";
-}
+        else { //If user input is valid, the function swaps the number on the board to the users character (X or O depending on the player).
+            for (int i = 0; i < board.size(); i++) {
+                if (('0' + player_input) == board[i]) {
+                board[i] = player;
+                input_exists = true;
+                }
+            }
+         }
+
+        if (!input_exists) {
+        std::cout << "\n Box is already filled! Try again!\n";
+        }
 
     } while (player_input == 0 || player_input > 9 || input_exists == false);
 }
@@ -87,14 +75,14 @@ void display_board() { //prints the board
 }
 
 
-bool check_winner(int p) {
+bool check_winner(int p) { //checks if one of the possible win conditions are met, and if it's met, it returns a "true" value for use in calculate_winner.
 
     char is_X_or_O;
 
-    if (p == 1) {
+    if (p == 1) { //checks player 1s inputs if they have won in one of the possible conditions
         is_X_or_O = 'X';
     }
-    else
+    else //checks player 2s or AIs inputs if they have won in one of the possible conditions
         is_X_or_O = 'O';
 
     if ((board[0] == is_X_or_O) && (board[1] == is_X_or_O) && (board[2] == is_X_or_O)) {
@@ -126,7 +114,7 @@ bool check_winner(int p) {
     return false;
 }
 
-int calculate_winner() {
+int calculate_winner() { //uses the results of check_winner to change the bool value game_won to true.
 
     if (check_winner(1)) {
         game_won = true;
@@ -140,7 +128,7 @@ int calculate_winner() {
 }
 
 void ai_player() {
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));  //creates a random seed based on the system clock.
     int ai_input = 0;
     bool input_exists = false;
     do {
@@ -151,7 +139,7 @@ void ai_player() {
                     if (('0' + ai_input) == board[i]) {
                         board[i] = player;
                         input_exists = true;
-                        std::cout << "AI player chose: " << ai_input << std::endl;
+                        std::cout << " AI player chose: " << ai_input << std::endl;
                     }
                 }
             }
@@ -159,87 +147,127 @@ void ai_player() {
     } while (ai_input == 0 || ai_input > 9 || input_exists == false);
 }
 
+void replay() { //asks the user if they want to replay, as well as resets the values back to their original state
+ 
+    int replay_choice = 0;
+
+    if (game_won == true || game_draw == true) {
+        do{
+            std::cout << "\n\n Would you like to replay? \n\t 1. Yes \n\t 2. No \n Type your choice here: ";
+            std::cin >> replay_choice;
+
+            if (replay_choice == 0 || replay_choice > 2) {
+                std::cout << "\n Invalid input. Please type your choice again!\n";
+            }
+        } while (replay_choice == 0 || replay_choice > 2);
+
+        if (replay_choice == 1) {
+            replay_game = true;
+            board = { '1','2','3','4','5','6','7','8','9' };
+            player = 'O';
+            game_won = false;
+            game_draw = false;
+            system("CLS");
+
+        }
+        else {
+            system("CLS");
+            replay_game = false;
+            std::cout << "\n Thanks for playing! :)\n";
+        }
+    }
+}
+
+
 
 int main()
 {
-    //start screen - Asks the user how many will be playing, and transfers the user to the version of the game they asked for.
-
-    std::cout << "***********Tic-Tac-Toe!***********";
-
     do {
-        std::cout << "\n\n Welcome to Tic-Tac-Toe!" << std::endl;
-        display_board();
-        std::cout << "\n How many players?\n Type 1 for 1 player, Type 2 for 2 players: ";
-        std::cin >> number_of_players;
-        if (number_of_players == 0 || number_of_players > 2) {
-            std::cout << "\n Invalid input. Try again.\n";
-        }
-    } while (number_of_players == 0 || number_of_players > 2);
+        //start screen - Asks the user how many will be playing, and transfers the user to the version of the game they asked for.
+
+        std::cout << "***********Tic-Tac-Toe!***********";
 
 
-    system("CLS");
-
-    //game screen one player
-    if (number_of_players == 1) {
-        std::cout << "***********Tic-Tac-Toe!***********\n\n";
-        std::cout << "\n Player 1 is [X], AI is [O]\n"; //lets the player know which symbol belongs to them.
-
-        int turn = 0;
-        display_board();
         do {
-            turn++;
-            set_player();
-            if (player == 'X') {
-                user_input();
+            std::cout << "\n\n Welcome to Tic-Tac-Toe!" << std::endl;
+            display_board();
+            std::cout << "\n How many players?\n Type 1 for 1 player, Type 2 for 2 players: ";
+            std::cin >> number_of_players;
+            if (number_of_players == 0 || number_of_players > 2) {
+                std::cout << "\n Invalid input. Try again.\n";
             }
-            else{
-                ai_player();
-            }
+        } while (number_of_players == 0 || number_of_players > 2); 
+
+        system("CLS");
+
+        //game screen one player
+
+        if (number_of_players == 1) {
             
-            if (turn > 4) {
-                int winner = calculate_winner();
-                if (winner == 1) {
-                    std::cout << "\n Player 1 is the winner!";
-                }
-                else if (winner == 2) {
-                    std::cout << "\n The computer is the winner!";
-                }
-                else if (turn == 9) {
-                    std::cout << "\n The game has reached a draw. \n Game over";
-                    game_draw = true;
-                }
-            }
-            display_board();
-        } while (!game_won && !game_draw);
-    }
-    //game screen two players
+            std::cout << "***********Tic-Tac-Toe!***********\n\n";
+            std::cout << "\n Player 1 is [X], AI is [O]\n"; //lets the player know which symbol belongs to them.
 
-    else if (number_of_players == 2) {
-        std::cout << "***********Tic-Tac-Toe!***********\n\n";
-        std::cout << "\n Player 1 is [X], Player 2 is [O]\n"; //lets the player know which symbol belongs to them.
-
-        int turn = 0;
-        display_board();
-        do {
-            turn++;
-            set_player();
-            user_input();
-            if (turn > 4) {
-                int winner = calculate_winner();
-                if (winner == 1) {
-                    std::cout << "\n Player 1 is the winner!";
-                }
-                else if (winner == 2) {
-                    std::cout << "\n Player 2 is the winner!";
-                }
-                else if (turn == 9) {
-                    std::cout << "\n The game has reached a draw. \n Game over";
-                    game_draw = true;
-                }
-            }
+            int turn = 0;
             display_board();
-        } while (!game_won && !game_draw);
-    }
+            do {
+                turn++;
+                set_player(); //sets the player turn
+                if (player == 'X') { //checks if it's the players turn or the AIs turn.
+                    user_input(); //asks the user for input, and lets the user input. Also checks if input is valid, or it exists.
+                }
+                else {
+                    ai_player(); //asks the AI for input. Input is randomized from the AI, and the function also checks if input is valid, or it already exists.
+                }
+
+                if (turn > 4) { //it's only possible to win after 5 moves, so the game starts to check if a winner exists
+                    int winner = calculate_winner();
+                    if (winner == 1) {
+                        std::cout << "\n Player 1 is the winner!\n";
+                    }
+                    else if (winner == 2) {
+                        std::cout << "\n The computer is the winner!\n";
+                    }
+                    else if (turn == 9) { //if the number of turns have reached 9, there are no possible moves to input on the board. Game outputs a message letting the users know they've reached a draw. and ends the game.
+                        std::cout << "\n The game has reached a draw. \n Game over\n";
+                        game_draw = true;
+                    }
+                }
+                display_board();
+            } while (!game_won && !game_draw); //loops the game while in progress.
+        }
+
+        //game screen two players
+
+        else if (number_of_players == 2) {
+            std::cout << "***********Tic-Tac-Toe!***********\n\n";
+            std::cout << "\n Player 1 is [X], Player 2 is [O]\n"; //lets the player know which symbol belongs to them.
+
+            int turn = 0;
+            display_board();
+            do {
+                turn++;
+                set_player(); //sets the player turn
+                user_input(); //asks the user for input, and lets the user input. Also checks if input is valid, or it exists
+                if (turn > 4) { //it's only possible to win after 5 moves, so the game starts to check if a winner exists
+                    int winner = calculate_winner();
+                    if (winner == 1) {
+                        std::cout << "\n Player 1 is the winner!";
+                    }
+                    else if (winner == 2) {
+                        std::cout << "\n Player 2 is the winner!";
+                    }
+                    else if (turn == 9) { //if the number of turns have reached 9, there are no possible moves to input on the board. Game outputs a message letting the users know they've reached a draw. and ends the game.
+                        std::cout << "\n The game has reached a draw. \n Game over";
+                        game_draw = true;
+                    }
+                }
+                display_board();
+            } while (!game_won && !game_draw); //loops the game while in progress.
+        }
+
+        replay(); //asks the user if they want to replay, and if they do, the user is sent back to the start screen. If they don't want to, it ends the program. 
+        
+    } while (replay_game == true); 
 }
 
 
